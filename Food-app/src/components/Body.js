1d10aa080b73430fe0ba56b3aa0572cd258fa9a7
invicibleshark <keshavnarkudi73@gmail.com>
2024-03-15 23:42:16 +0530

    Added Shimmer Ui and Used usestate,useEffect Hooks

--- Food-app/src/components/Body.js
+++ Food-app/src/components/Body.js
@@ -1,11 +1,25 @@
 import Home from "./index.js"
 import restaurantList from "../utils/Mockdata.js";
-import { useState } from "react";
-
+import { useState, useEffect} from "react";
+import Loader from "./Shimmerui.js";
 
 const Body=()=>{
-    let [ListofRestaurants,setListofRestaurants]=useState(restaurantList);
-    return (
+    const fetchData=async()=>{
+        const data=await fetch
+        ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        console.log(json);
+        setListofRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    
+    }
+    let [ListofRestaurants,setListofRestaurants]=useState([]);
+    useEffect(()=>{
+        fetchData();
+    },[]);
+    // if(ListofRestaurants.length ===0){
+    //     return(<Loader/>);
+    // }
+    return (ListofRestaurants.length ===0)?<Loader/>:(
         <div className="bodyContainer">
         <div className="Search">
             <h1>This is Search</h1>
---
+++ Food-app/src/components/Shimmerui.js
@@ -0,0 +1,20 @@
+const Loader=()=>{
+    return (
+        <div className="LoaderContainer">
+            <div className="Loader"><div className="ImageCont"></div></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+            <div className="Loader"></div>
+        </div>
+    )
+};
+export default Loader;
